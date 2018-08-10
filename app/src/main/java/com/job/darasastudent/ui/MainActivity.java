e430308f283bd0e9912d3b7b52dab762aad6d8d2
JobGetabu <getabujob@gmail.com>
2018-08-10 18:14:17 +0300

    repackaged the project

--- app/src/main/java/com/job/darasastudent/ui/MainActivity.java
+++ app/src/main/java/com/job/darasastudent/ui/MainActivity.java
@@ -1,7 +1,9 @@
-package com.job.darasastudent;
+package com.job.darasastudent.ui;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+
+import com.job.darasastudent.R;
 
 public class MainActivity extends AppCompatActivity {
 
