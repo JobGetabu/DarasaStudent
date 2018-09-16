ce16c4b9b2de106926691c3ba0b35d2a13c80893
JobGetabu <getabujob@gmail.com>
2018-09-16 15:09:41 +0300

    added few util functions to the codebase

---
+++ app/src/main/java/com/job/darasastudent/util/Constants.java
@@ -0,0 +1,25 @@
+package com.job.darasastudent.util;
+
+/**
+ * Created by Job on Friday : 8/3/2018.
+ */
+public class Constants {
+    public static final String LECUSERCOL = "LecUser";
+    public static final String LECTEACHCOL = "LecTeach";
+    public static final String LECTEACHTIMECOL = "LecTeachTime";
+    public static final String LECTEACHCOURSESUBCOL = "Courses";
+    public static final String LECAUTHCOL = "LecAuth";
+    public static final String STUDENTDETAILSCOL = "StudentDetails";
+
+    //for future  when we will access even finer location details
+    public static final int SUCCESS_RESULT = 0;
+    public static final int FAILURE_RESULT = 1;
+    public static final String PACKAGE_NAME = "com.job.hacelaapp.util";
+    public static final String RECEIVER = PACKAGE_NAME + ".RECEIVER";
+    public static final String RESULT_DATA_KEY = PACKAGE_NAME +
+            ".RESULT_DATA_KEY";
+    public static final String LOCATION_DATA_EXTRA = PACKAGE_NAME +
+            ".LOCATION_DATA_EXTRA";
+    public static final int LOCATION_INTERVAL = 10000;
+    public static final int FASTEST_LOCATION_INTERVAL = 5000;
+}
