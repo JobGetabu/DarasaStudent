33768930e44d09f5e7d229f161ab72572963ee35
JobGetabu <getabujob@gmail.com>
2018-09-17 15:50:16 +0300

    added main custom float fab button designed

--- app/src/main/java/com/job/darasastudent/ui/MainActivity.java
+++ app/src/main/java/com/job/darasastudent/ui/MainActivity.java
@@ -1,18 +1,32 @@
 package com.job.darasastudent.ui;
 
 import android.content.Intent;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 
 import com.job.darasastudent.R;
+import com.robertlevonyan.views.customfloatingactionbutton.FloatingActionButton;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
+import butterknife.OnClick;
 
 public class MainActivity extends AppCompatActivity {
+
+    @BindView(R.id.main_fab)
+    FloatingActionButton mainFab;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        ButterKnife.bind(this);
 
+    }
+
+    @OnClick(R.id.main_fab)
+    public void onFabClicked() {
         startActivity(new Intent(this,ScanActivity.class));
+
     }
 }
