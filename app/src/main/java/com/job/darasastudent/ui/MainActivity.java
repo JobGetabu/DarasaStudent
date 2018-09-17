05b9849a9d23e9f0be39f82f7e9040fa7373babc
JobGetabu <getabujob@gmail.com>
2018-09-17 19:17:17 +0300

    added few resources and designed :) screens

--- app/src/main/java/com/job/darasastudent/ui/MainActivity.java
+++ app/src/main/java/com/job/darasastudent/ui/MainActivity.java
@@ -1,14 +1,22 @@
 package com.job.darasastudent.ui;
 
+import android.app.DatePickerDialog;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.bottomappbar.BottomAppBar;
 import android.support.v7.app.AppCompatActivity;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.DatePicker;
+import android.widget.Toast;
 
 import com.job.darasastudent.R;
 import com.robertlevonyan.views.customfloatingactionbutton.FloatingActionButton;
+
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.Date;
+import java.util.Locale;
 
 import butterknife.BindView;
 import butterknife.ButterKnife;
@@ -20,6 +28,7 @@ public class MainActivity extends AppCompatActivity {
     FloatingActionButton mainFab;
     @BindView(R.id.main_bar)
     BottomAppBar bar;
+    private Date mDate;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -32,12 +41,12 @@ public class MainActivity extends AppCompatActivity {
 
     @OnClick(R.id.main_fab)
     public void onFabClicked() {
-        startActivity(new Intent(this,ScanActivity.class));
+        startActivity(new Intent(this, ScanActivity.class));
     }
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
-        //getMenuInflater().inflate(R.menu.bottom_nav_drawer_menu, menu);
+        getMenuInflater().inflate(R.menu.menu_calender, menu);
         return super.onCreateOptionsMenu(menu);
     }
 
@@ -49,6 +58,35 @@ public class MainActivity extends AppCompatActivity {
             case android.R.id.home:
                 BottomNavigationDrawerFragment bottomNavDrawerFragment = new BottomNavigationDrawerFragment();
                 bottomNavDrawerFragment.show(getSupportFragmentManager(), BottomNavigationDrawerFragment.TAG);
+                break;
+
+            case R.id.menu_calender:
+
+                final Calendar myCalendar = Calendar.getInstance();
+                final DatePickerDialog.OnDateSetListener date = new DatePickerDialog.OnDateSetListener() {
+
+                    @Override
+                    public void onDateSet(DatePicker view, int year, int monthOfYear,
+                                          int dayOfMonth) {
+                        // TODO Auto-generated method stub
+                        myCalendar.set(Calendar.YEAR, year);
+                        myCalendar.set(Calendar.MONTH, monthOfYear);
+                        myCalendar.set(Calendar.DAY_OF_MONTH, dayOfMonth);
+
+
+                        mDate = myCalendar.getTime();
+                        String myFormat = "MM/dd/yy"; //In which you need put here
+                        SimpleDateFormat sdf = new SimpleDateFormat(myFormat, Locale.ENGLISH);
+
+
+                        Toast.makeText(MainActivity.this, sdf.format(myCalendar.getTime()) , Toast.LENGTH_SHORT).show();
+                    }
+                };
+
+                new DatePickerDialog(MainActivity.this, date, myCalendar
+                        .get(Calendar.YEAR), myCalendar.get(Calendar.MONTH),
+                        myCalendar.get(Calendar.DAY_OF_MONTH)).show();
+
                 break;
         }
 
