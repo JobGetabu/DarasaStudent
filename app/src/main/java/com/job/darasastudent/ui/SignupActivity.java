b9f56cb323df569daddafd7b90f1cb797b2828bd
JobGetabu <getabujob@gmail.com>
2018-09-28 03:37:48 +0300

    sign up logic

--- app/src/main/java/com/job/darasastudent/ui/SignupActivity.java
+++ app/src/main/java/com/job/darasastudent/ui/SignupActivity.java
@@ -1,24 +1,38 @@
 package com.job.darasastudent.ui;
 
 import android.content.Intent;
+import android.graphics.Color;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
+import android.support.design.widget.Snackbar;
 import android.support.design.widget.TextInputLayout;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+import android.view.View;
 import android.widget.Button;
 import android.widget.LinearLayout;
 
 import com.google.android.gms.auth.api.signin.GoogleSignIn;
+import com.google.android.gms.auth.api.signin.GoogleSignInAccount;
 import com.google.android.gms.auth.api.signin.GoogleSignInClient;
 import com.google.android.gms.auth.api.signin.GoogleSignInOptions;
+import com.google.android.gms.common.api.ApiException;
+import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.OnFailureListener;
 import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthCredential;
+import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.auth.GoogleAuthProvider;
 import com.google.firebase.firestore.DocumentReference;
+import com.google.firebase.firestore.DocumentSnapshot;
 import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.iid.FirebaseInstanceId;
 import com.job.darasastudent.R;
 import com.job.darasastudent.model.StudentDetails;
+import com.job.darasastudent.util.AppStatus;
 import com.job.darasastudent.util.DoSnack;
 
 import butterknife.BindView;
@@ -73,12 +87,94 @@ public class SignupActivity extends AppCompatActivity {
 
     @OnClick(R.id.signup_button)
     public void onSignupButtonClicked() {
+
+        if (!AppStatus.getInstance(getApplicationContext()).isOnline()) {
+
+            doSnack.showSnackbar("You're offline", "Retry", new View.OnClickListener() {
+                @Override
+                public void onClick(View view) {
+                    onSignupButtonClicked();
+                }
+            });
+
+            return;
+        }
+
+
+        String email = signupEmail.getEditText().getText().toString();
+        String password = signupPassword.getEditText().getText().toString();
+
+        if (validate()) {
+
+            final SweetAlertDialog pDialog = new SweetAlertDialog(this, SweetAlertDialog.PROGRESS_TYPE);
+            pDialog.getProgressHelper().setBarColor(Color.parseColor("#FF5521"));
+            pDialog.setTitleText("Registration...");
+            pDialog.setCancelable(false);
+            pDialog.show();
+
+            mAuth.createUserWithEmailAndPassword(email, password)
+                    .addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+                        @Override
+                        public void onComplete(@NonNull Task<AuthResult> authtask) {
+                            if (authtask.isSuccessful()) {
+
+                                //registration successful
+
+                                String devicetoken = FirebaseInstanceId.getInstance().getToken();
+                                String mCurrentUserid = FirebaseAuth.getInstance().getCurrentUser().getUid();
+
+                                writingToStudUsers(pDialog, devicetoken, FirebaseAuth.getInstance().getCurrentUser(),mCurrentUserid);
+
+
+                            } else {
+                                pDialog.dismiss();
+                                doSnack.UserAuthToastExceptions(authtask);
+                            }
+                        }
+                    });
+        }
     }
 
     @OnClick({R.id.signup_via_google,R.id.signup_via_google})
     public void onSignupViaGoogleClicked() {
+
+        Intent signInIntent = mGoogleSignInClient.getSignInIntent();
+        startActivityForResult(signInIntent, RC_SIGN_IN);
+        signupViaGoogle.setEnabled(false);
+
     }
 
+    private void sendToMain() {
+
+        Intent mainIntent = new Intent(this, MainActivity.class);
+        mainIntent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TASK);
+        mainIntent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+        startActivity(mainIntent);
+        finish();
+    }
+
+    private void updateTokenOnly(String mCurrentUserid, String device_token, final SweetAlertDialog pDialog) {
+        //update device token
+
+        // Set the value of 'Users'
+        DocumentReference usersRef = mFirestore.collection(STUDENTDETAILSCOL).document(mCurrentUserid);
+
+        usersRef.update("devicetoken", device_token)
+                .addOnSuccessListener(new OnSuccessListener<Void>() {
+                    @Override
+                    public void onSuccess(Void aVoid) {
+                        pDialog.dismissWithAnimation();
+                        sendToMain();
+                    }
+                }).addOnFailureListener(new OnFailureListener() {
+            @Override
+            public void onFailure(@NonNull Exception e) {
+                pDialog.dismiss();
+                doSnack.errorPrompt("Oops...", e.getMessage());
+            }
+        });
+
+    }
 
     private void writingToStudUsers(final SweetAlertDialog pDialog, String device_token, FirebaseUser user, String mCurrentUserid) {
 
@@ -138,4 +234,94 @@ public class SignupActivity extends AppCompatActivity {
     private boolean isEmailValid(CharSequence email) {
         return android.util.Patterns.EMAIL_ADDRESS.matcher(email).matches();
     }
+
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        //Result returned from launching the Intent from GoogleSignInApi.getSignInIntent(...);
+
+        if (requestCode == RC_SIGN_IN) {
+            Task<GoogleSignInAccount> task = GoogleSignIn.getSignedInAccountFromIntent(data);
+            try {
+                // Google Sign In was successful, authenticate with Firebase
+                GoogleSignInAccount account = task.getResult(ApiException.class);
+                firebaseAuthWithGoogle(account);
+            } catch (ApiException e) {
+                // Google Sign In failed, update UI appropriately
+                Log.w(TAG, "Google sign in failed", e);
+                // ...
+                Snackbar.make(findViewById(android.R.id.content), "Google sign in failed", Snackbar.LENGTH_LONG).show();
+                signupViaGoogle.setEnabled(true);
+            }
+        }
+    }
+
+    private void firebaseAuthWithGoogle(GoogleSignInAccount acct) {
+        Log.d(TAG, "firebaseAuthWithGoogle:" + acct.getId());
+
+        final SweetAlertDialog pDialog = new SweetAlertDialog(this, SweetAlertDialog.PROGRESS_TYPE);
+        pDialog.getProgressHelper().setBarColor(Color.parseColor("#FF5521"));
+        pDialog.setTitleText("Buckle up...");
+        pDialog.setCancelable(false);
+        pDialog.show();
+
+        AuthCredential credential = GoogleAuthProvider.getCredential(acct.getIdToken(), null);
+        mAuth.signInWithCredential(credential)
+                .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if (task.isSuccessful()) {
+                            // Sign in success, update UI with the signed-in user's information
+                            Log.d(TAG, "signInWithCredential:success");
+                            final FirebaseUser user = mAuth.getCurrentUser();
+
+                            final String device_token = FirebaseInstanceId.getInstance().getToken();
+                            final String mCurrentUserid = mAuth.getCurrentUser().getUid();
+
+                            // refactor this not to write to DB each time...check if account exists
+
+                            DocumentReference docReference = mFirestore.collection(STUDENTDETAILSCOL).document(mCurrentUserid);
+                            docReference.get()
+                                    .addOnCompleteListener(new OnCompleteListener<DocumentSnapshot>() {
+                                        @Override
+                                        public void onComplete(@NonNull Task<DocumentSnapshot> task) {
+                                            if (task.isSuccessful()) {
+                                                DocumentSnapshot document = task.getResult();
+                                                if (document.exists()) {
+                                                    Log.d(TAG, "DocumentSnapshot data: " + document.getData());
+
+                                                    //update token only
+                                                    updateTokenOnly(mCurrentUserid, device_token, pDialog);
+
+                                                } else {
+                                                    Log.d(TAG, "No such document");
+
+                                                    //logging in with no pre account
+                                                    //region create fresh account
+
+                                                    pDialog.changeAlertType(SweetAlertDialog.SUCCESS_TYPE);
+                                                    pDialog.setTitleText("Account doesn't exists! \n Creating one...");
+
+                                                    //write to db
+                                                    writingToStudUsers(pDialog, device_token, user, mCurrentUserid);
+
+                                                    //endregion
+                                                }
+                                            } else {
+                                                Log.d(TAG, "get failed with ", task.getException());
+                                            }
+                                        }
+                                    });
+                        } else {
+                            // If sign in fails, display a message to the user.
+                            pDialog.dismissWithAnimation();
+                            Log.w(TAG, "signInWithCredential:failure", task.getException());
+                            Snackbar.make(findViewById(android.R.id.content), "Authentication Failed.", Snackbar.LENGTH_SHORT).show();
+                            signupViaGoogle.setEnabled(true);
+                        }
+                    }
+                });
+    }
+
 }
