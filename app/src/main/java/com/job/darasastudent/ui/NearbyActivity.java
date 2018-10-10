268a1cfc4f94a0bfd5389b5d73bdea66f0afe56b
JobGetabu <getabujob@gmail.com>
2018-10-10 19:01:14 +0300

    duplicated nearby activity from lec app

---
+++ app/src/main/java/com/job/darasastudent/ui/NearbyActivity.java
@@ -0,0 +1,217 @@
+package com.job.darasastudent.ui;
+
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+import com.google.android.gms.nearby.Nearby;
+import com.google.android.gms.nearby.connection.AdvertisingOptions;
+import com.google.android.gms.nearby.connection.ConnectionInfo;
+import com.google.android.gms.nearby.connection.ConnectionLifecycleCallback;
+import com.google.android.gms.nearby.connection.ConnectionResolution;
+import com.google.android.gms.nearby.connection.ConnectionsClient;
+import com.google.android.gms.nearby.connection.ConnectionsStatusCodes;
+import com.google.android.gms.nearby.connection.DiscoveredEndpointInfo;
+import com.google.android.gms.nearby.connection.DiscoveryOptions;
+import com.google.android.gms.nearby.connection.EndpointDiscoveryCallback;
+import com.google.android.gms.nearby.connection.Payload;
+import com.google.android.gms.nearby.connection.PayloadCallback;
+import com.google.android.gms.nearby.connection.PayloadTransferUpdate;
+import com.google.android.gms.nearby.connection.Strategy;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.job.darasastudent.R;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
+import butterknife.OnClick;
+
+public class NearbyActivity extends AppCompatActivity {
+
+    private static final String TAG = "NearbyActivity";
+    private static final Strategy STRATEGY = Strategy.P2P_STAR;
+
+    @BindView(R.id.cnt_status)
+    TextView cntStatus;
+    @BindView(R.id.btn_cnt)
+    Button btnCnt;
+    @BindView(R.id.btn_disconnect)
+    Button btnDisconnect;
+    @BindView(R.id.cnt_device_info)
+    TextView cntDeviceInfo;
+    private String SERVICE_ID = "com.job.darasalecturer";
+    private StringBuilder stringBuilder;
+
+    // Our handle to Nearby Connections
+    private ConnectionsClient connectionsClient;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_nearby);
+        ButterKnife.bind(this);
+
+        connectionsClient = Nearby.getConnectionsClient(this);
+
+
+        stringBuilder.append("");
+        btnDisconnect.setVisibility(View.GONE);
+    }
+
+    @OnClick(R.id.btn_cnt)
+    public void onBtnCntClicked() {
+
+        startAdvertising();
+        startDiscovery();
+        cntStatus.setText("Searching students");
+
+
+    }
+
+    @OnClick(R.id.btn_disconnect)
+    public void onBtnDisconnectClicked() {
+
+
+    }
+
+    private void startAdvertising() {
+        connectionsClient.startAdvertising(
+                getUserNickname(),
+                SERVICE_ID,
+                mConnectionLifecycleCallback,
+                new AdvertisingOptions.Builder().setStrategy(STRATEGY).build())
+                .addOnSuccessListener(new OnSuccessListener<Void>() {
+                    @Override
+                    public void onSuccess(Void aVoid) {
+                        // We're advertising!
+
+                    }
+                })
+                .addOnFailureListener(new OnFailureListener() {
+                    @Override
+                    public void onFailure(@NonNull Exception e) {
+                        // We were unable to start advertising.
+                        Log.w(TAG, "onFailure: We were unable to start advertising.");
+
+                    }
+                });
+    }
+
+    private String getUserNickname() {
+        return "dummy lec user";
+    }
+
+    private final EndpointDiscoveryCallback mEndpointDiscoveryCallback =
+            new EndpointDiscoveryCallback() {
+                @Override
+                public void onEndpointFound(
+                        String endpointId, final DiscoveredEndpointInfo discoveredEndpointInfo) {
+                    // An endpoint was found!
+                    Log.i(TAG, "onEndpointFound: endpoint found, connecting");
+                    connectionsClient.requestConnection(getUserNickname(), endpointId, mConnectionLifecycleCallback)
+                            .addOnSuccessListener(new OnSuccessListener<Void>() {
+                                @Override
+                                public void onSuccess(Void aVoid) {
+
+                                    // We successfully requested a connection. Now both sides
+                                    // must accept before the connection is established.
+                                    //TODO: List the discovered devices here
+
+                                    stringBuilder.append("device: -> " + discoveredEndpointInfo.getEndpointName());
+                                    cntDeviceInfo.setText(stringBuilder);
+
+                                }
+                            }).addOnFailureListener(
+                            new OnFailureListener() {
+                                @Override
+                                public void onFailure(@NonNull Exception e) {
+                                    // Nearby Connections failed to request the connection.
+                                }
+                            });
+
+                }
+
+                @Override
+                public void onEndpointLost(String endpointId) {
+                    // A previously discovered endpoint has gone away.
+                }
+            };
+
+    private void startDiscovery() {
+        connectionsClient.startDiscovery(
+                SERVICE_ID,
+                mEndpointDiscoveryCallback,
+                new DiscoveryOptions.Builder().setStrategy(STRATEGY).build())
+                .addOnSuccessListener(
+                        new OnSuccessListener<Void>() {
+                            @Override
+                            public void onSuccess(Void unusedResult) {
+                                // We're discovering!
+                            }
+                        })
+                .addOnFailureListener(
+                        new OnFailureListener() {
+                            @Override
+                            public void onFailure(@NonNull Exception e) {
+                                // We were unable to start discovering.
+                                Log.w(TAG, "onFailure: We were unable to start discovering.");
+                            }
+                        });
+    }
+
+    private final ConnectionLifecycleCallback mConnectionLifecycleCallback =
+            new ConnectionLifecycleCallback() {
+
+                @Override
+                public void onConnectionInitiated(
+                        String endpointId, ConnectionInfo connectionInfo) {
+                    // Automatically accept the connection on both sides.
+                    connectionsClient.acceptConnection(endpointId, mPayloadCallback);
+
+                    //TODO: handle disconnect for "unverified" students
+                }
+
+                @Override
+                public void onConnectionResult(String endpointId, ConnectionResolution result) {
+                    switch (result.getStatus().getStatusCode()) {
+                        case ConnectionsStatusCodes.STATUS_OK:
+                            // We're connected! Can now start sending and receiving data.
+                            break;
+                        case ConnectionsStatusCodes.STATUS_CONNECTION_REJECTED:
+                            // The connection was rejected by one or both sides.
+                            break;
+                        case ConnectionsStatusCodes.STATUS_ERROR:
+                            // The connection broke before it was able to be accepted.
+                            break;
+                    }
+                }
+
+                @Override
+                public void onDisconnected(String endpointId) {
+                    // We've been disconnected from this endpoint. No more data can be
+                    // sent or received.
+                }
+            };
+
+    // Callbacks for receiving payloads
+    private final PayloadCallback mPayloadCallback =
+            new PayloadCallback() {
+                @Override
+                public void onPayloadReceived(String endpointId, Payload payload) {
+                }
+                @Override
+                public void onPayloadTransferUpdate(String endpointId, PayloadTransferUpdate update) {
+                }
+            };
+
+    @Override
+    protected void onStop() {
+        connectionsClient.stopAllEndpoints();
+
+        super.onStop();
+    }
+}
