d34eb9364dfd995e783d131d0ccb4b38bd4a0c50
JobGetabu <getabujob@gmail.com>
2018-10-15 15:34:58 +0300

    added a notification service

---
+++ app/src/main/java/com/job/darasastudent/services/MyFirebaseMessagingService.java
@@ -0,0 +1,9 @@
+package com.job.darasastudent.services;
+
+import com.google.firebase.messaging.FirebaseMessagingService;
+
+/**
+ * Created by Job on Monday : 10/15/2018.
+ */
+public class MyFirebaseMessagingService extends FirebaseMessagingService {
+}
