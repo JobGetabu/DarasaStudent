ba6eb8c40bb3f21f54c9cbd784ac3ed269c854f2
Job Getabu <getabujob@gmail.com>
2018-11-25 18:50:33 +0300

    updated NotificationUtil to latest

--- app/src/main/java/com/job/darasastudent/util/NotificationUtil.java
+++ app/src/main/java/com/job/darasastudent/util/NotificationUtil.java
@@ -1,6 +1,7 @@
 package com.job.darasastudent.util;
 
 import android.app.Notification;
+import android.app.NotificationChannel;
 import android.app.NotificationManager;
 import android.app.PendingIntent;
 import android.content.Context;
@@ -13,7 +14,6 @@ import android.support.v4.content.ContextCompat;
 import com.job.darasastudent.R;
 import com.job.darasastudent.ui.MainActivity;
 
-
 /**
  * Created by Job on Monday : 7/16/2018.
  */
@@ -24,10 +24,10 @@ public class NotificationUtil {
     public static final String LABEL_REPLY = "Reply";
     public static final String LABEL_ARCHIVE = "Archive";
     public static final String KEY_PRESSED_ACTION = "KEY_PRESSED_ACTION";
-    public static final String REPLY_ACTION = "com.job.darasastudent.util.ACTION_MESSAGE_REPLY";
+    public static final String REPLY_ACTION = "com.job.darasalecturer.util.ACTION_MESSAGE_REPLY";
 
 
-    public void showStandardHeadsUpNotification(Context context,String title, String message) {
+    public void showStandardHeadsUpNotification(Context context, String title, String message) {
 
         PendingIntent archiveIntent = PendingIntent.getActivity(context,
                 ARCHIVE_INTENT_ID,
@@ -44,7 +44,7 @@ public class NotificationUtil {
 
         NotificationCompat.Builder notificationBuider = createNotificationBuider(
                 context, title, message);
-        notificationBuider.setPriority(Notification.PRIORITY_HIGH).setVibrate(new long[0]);
+        notificationBuider.setPriority(Notification.PRIORITY_HIGH).setVibrate(new long[1]);
         /*notificationBuider.addAction(replyAction);
         notificationBuider.addAction(archiveAction);*/
 
@@ -55,25 +55,41 @@ public class NotificationUtil {
         PendingIntent fullScreenPendingIntent = PendingIntent.getActivity(context, 0,
                 push, PendingIntent.FLAG_CANCEL_CURRENT);
         notificationBuider.setFullScreenIntent(fullScreenPendingIntent, true);
-        showNotification(context, notificationBuider.build(), 0);
+
+        NotificationManager mNotificationManager =
+                (NotificationManager) context.getSystemService(Context.NOTIFICATION_SERVICE);
+
+        //If on Oreo then notification required a notification channel.
+        if (android.os.Build.VERSION.SDK_INT >= android.os.Build.VERSION_CODES.O) {
+            NotificationChannel channel = new NotificationChannel("default", "Default", NotificationManager.IMPORTANCE_DEFAULT);
+            mNotificationManager.createNotificationChannel(channel);
+        }
+
+        showNotification(context, notificationBuider.build(), 0, mNotificationManager);
     }
 
-    public NotificationCompat.Builder createNotificationBuider(Context context,
-                                                               String title, String message) {
+    private NotificationCompat.Builder createNotificationBuider(Context context,
+                                                                String title, String message) {
         Bitmap largeIcon = BitmapFactory.decodeResource(context.getResources(),
                 R.mipmap.ic_launcher);
+
+
+        //NotificationManager notificationManager = (NotificationManager) context.getSystemService(Context.NOTIFICATION_SERVICE);
+
+
+
         return new NotificationCompat.Builder(context)
                 .setSmallIcon(R.drawable.ic_launcher)
                 .setContentTitle(title)
                 .setContentText(message)
                 .setLargeIcon(largeIcon)
+                .setSmallIcon(R.drawable.ic_classroom)
                 .setColor(ContextCompat.getColor(context, R.color.colorPrimary))
                 .setAutoCancel(true);
     }
 
-    private void showNotification(Context context, Notification notification, int id) {
-        NotificationManager mNotificationManager =
-                (NotificationManager) context.getSystemService(Context.NOTIFICATION_SERVICE);
+    private void showNotification(Context context, Notification notification, int id, NotificationManager mNotificationManager) {
+
         mNotificationManager.notify(id, notification);
     }
 
@@ -83,4 +99,5 @@ public class NotificationUtil {
                 .setAction(REPLY_ACTION)
                 .putExtra(KEY_PRESSED_ACTION, label);
     }
+
 }
